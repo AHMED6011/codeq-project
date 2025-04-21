f03c28b573a68f1d3d35e69e4256dcb33f55c432
riad osman <riyados973@gmail.com>
2025-04-21 15:38:33 +0300

    💬 Update testimonial components and styles

--- src/components/Testimonial/Testimonial.jsx
+++ src/components/Testimonial/Testimonial.jsx
@@ -1,31 +1,44 @@
+"use client";
+import { useState, useEffect } from "react";
 import React from "react";
 import styles from "./testimonial.module.css";
 import TestimonialCard from "./TestimonialCard";
 import Image from "next/image";
 export default function Testimonials() {
+  const [activeIndex, setActiveIndex] = useState(0);
+
+  useEffect(() => {
+    const interval = setInterval(() => {
+      setActiveIndex((prevIndex) => (prevIndex + 1) % 3);
+    }, 4000);
+
+    return () => clearInterval(interval);
+  }, []);
   return (
-    <div className="my-5 text-center">
-      <h1 className="text-center" style={{marginTop:"150px"}}>آراء عملائنا</h1>
-      <p style={{ maxWidth: "611px", margin: "auto", fontWeight: "600" }}>
-        من خلال تعاوننا مع عملائنا، تمكنا من تحقيق رؤاهم وتحويلها إلى نتائج
-        ملموسة. اكتشف كيف أسهمت خدماتنا في نجاح مشاريعهم وتحقيق أهدافهم
-      </p>
-      <div
-        style={{
-          marginTop: "50px",
-          position: "relative",
-          width: "100%",
-          overflow: "hidden",
-          display: "flex",
-          justifyContent: "center",
-          flexDirection: "row",
-          alignItems: "center",
-          gap: "40px",
-        }}
-      >
-        <TestimonialCard />
-        <TestimonialCard />
-        <TestimonialCard />
+    <div
+    className="testimonial-container"
+    style={{
+      marginTop: "50px",
+      position: "relative",
+      width: "100%",
+      height: "400px",
+      overflow: "hidden",
+    }}
+  >
+    <div
+      className="testimonial-slider"
+      style={{
+        position: "absolute",
+        transform: `translateX(${activeIndex * 33.33}%)`,
+        transition: "transform 0.5s ease-in-out",
+        display: "flex",
+        gap: "40px",
+        width: "300%",
+      }}
+    >
+      <TestimonialCard />
+      <TestimonialCard />
+      <TestimonialCard />
       </div>
     </div>
   );
